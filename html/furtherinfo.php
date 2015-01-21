dcf2a51da75ab74b02e9a70834fe414373b060b2
D Hope <dictator_hope@hotmail.com>
2015-01-21 13:44:53 +0000

    Added phone number

--- html/furtherinfo.php
+++ html/furtherinfo.php
@@ -8,11 +8,13 @@ There are two costs associated with the app:
 </ul>
 The standard tablets we recommend are the 10 inch Samsung Galaxy Tab 4 (&pound;250-280) or the Google Nexus 9 (&pound;300) but there are cheaper Android tablets available which can be used instead. Where you already have suitable tablets then this cost won't apply.
 
-The monthly subscription is negotiable dependent on the number of children at a nursery and pays for the nursery and parent websites.
+The monthly subscription is negotiable dependent on the number of children at a nursery and the features you require.
  
 <h2>Brochure</h2>
 Click <a href="files/AtNurseryTodayBrochure.pdf">here</a> to download our flyer which summarises At Nursery Today
 <h2>Contact Us</h2>
 To start using At Nursery Today please get in touch
 
-If you want to learn more, get a demo or get started please email us at <a href="mailto:info@atnurserytoday.co.uk">info@atnurserytoday.co.uk</a>
+If you want to learn more, get a demo or get started please email us or call us at:
+<p class="contactDetails">Mail: <a href="mailto:info@atnurserytoday.co.uk">info@atnurserytoday.co.uk</a></p>
+<p class="contactDetails">Tel: 01904 416426</p>
